20f877481afe2e6deffcb46096568915d35c0882
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-19 16:37:07 -0400

    GamblingPlayer Interface

--- src/main/java/com/github/zipcodewilmington/utils/AnsiColor.java
+++ src/main/java/com/github/zipcodewilmington/utils/AnsiColor.java
@@ -4,7 +4,7 @@ package com.github.zipcodewilmington.utils;
  * Created by leon on 7/21/2020.
  * used to create color for output of `IOConsole`
  */
-public enum AnsiColor {
+public enum     AnsiColor {
     AUTO("\u001B[0m"),
     BLACK("\u001B[30m"),
     RED("\u001B[31m"),
