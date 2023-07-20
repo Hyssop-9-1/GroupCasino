4f4c244fd91dfa010ee9d9630e6f2eedf5a49c21
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-20 19:36:07 -0400

    WarGames

--- src/main/java/com/github/zipcodewilmington/casino/games/war/WarGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/war/WarGame.java
@@ -1,24 +1,24 @@
 package com.github.zipcodewilmington.casino.games.war;
 
 
+import com.github.zipcodewilmington.casino.CasinoAccount;
 import com.github.zipcodewilmington.casino.GameInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
 import com.github.zipcodewilmington.casino.gametools.Card;
 import com.github.zipcodewilmington.casino.gametools.Deck;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.github.zipcodewilmington.utils.AnsiColor;
+import com.github.zipcodewilmington.utils.IOConsole;
 
 
 public class WarGame implements GameInterface {
-
+    private final IOConsole console = new IOConsole(AnsiColor.CYAN);
     Deck warDeck = new Deck();
     int gamePoints = 0;
 
 //    private List<WarPlayer> players = new ArrayList<>();
 //    public WarGame(List<WarPlayer> players) { this.players.addAll(players);}
     private WarPlayer player;
-    private WarPlayer dealer = new WarPlayer(null);
+    private WarPlayer dealer = new WarPlayer(new CasinoAccount("Dealer",null));
 
     public WarGame() {
 
@@ -26,16 +26,42 @@ public class WarGame implements GameInterface {
 
     public void compareCards(Card playerCard, Card dealerCard){
 
+        switch (Integer.compare(playerCard.getValue(), dealerCard.getValue())){
+            case -1: //Dealer Wins
+                System.out.println("Dealer wins the hand.");
+                dealer.addPoints(gamePoints);
+                break;
+            case 1: //Player Wins
+                System.out.println(player.getAccountName()+ "wins the hand!");
+                player.addPoints(gamePoints);
+                break;
+            case 0: //War!!
+                System.out.println("Go to war!");
+                break;
+        }
+        gamePoints=0;
+    }
+    public void flipCards(WarPlayer player, int numCards){
+        int i =1;
+        do  {
+            player.getTopCard();
+            gamePoints += 1;
+            i++;
+        } while (i<=numCards);
+        System.out.println(player.getAccountName()+ " Card: "+player.getCurrentCard().getRank()+ " of "+player.getCurrentCard().getSuit());
+
     }
 
     @Override
     public void run() {
         setup();
         while (player.playerHand.deckSize() > 0){
-            player.getTopCard();
-            dealer.getTopCard();
-            gamePoints += 2;
+            String input = console.getStringInput(player.getAccountName()+", press [P] to play a card.");
+                if(input.equals("P")){
+                    flipCards(player,1);
+                    flipCards(dealer, 1);
 
+                }
 
 
 
