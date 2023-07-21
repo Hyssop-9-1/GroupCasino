ed6c90729c2d44d5a4500806a241cd3c0b01fc5a
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-21 13:23:48 -0400

    WarPlayer Tests

--- src/test/java/com/github/zipcodewilmington/casino/games/war/WarPlayerTest.java
+++ src/test/java/com/github/zipcodewilmington/casino/games/war/WarPlayerTest.java
@@ -22,30 +22,59 @@ public class WarPlayerTest {
     public void testGetTopCard(){
         CasinoAccount acct = new CasinoAccount(null, null);
         WarPlayer player = new WarPlayer(acct);
+        Deck deck = new Deck();
+        player.addPlayerHand(deck.deal(52));
+
 
         String expected = "TWO of SPADES";
         String actual = player.getTopCard().toString();
-//        Assert.assertEquals(expected, actual);
-        System.out.println(actual);
+        Assert.assertEquals(expected, actual);
+
     }
 
     @Test
     public void testGetCurrentCard(){
+        CasinoAccount acct = new CasinoAccount(null, null);
+        WarPlayer player = new WarPlayer(acct);
         Deck deck = new Deck();
+        player.addPlayerHand(deck.deal(52));
+
         String expected = "THREE of SPADES";
-        deck.removeTopCard();
-        deck.removeTopCard(); //remove the first and second cards
-//        String actual =
+        player.getTopCard();
+        player.getTopCard(); //remove the first and second cards
+        String actual = player.getCurrentCard().toString();
+
+        Assert.assertEquals(expected, actual);
 
 
     }
 
     @Test
     public void testAddPlayerHand(){
+        CasinoAccount acct = new CasinoAccount(null, null);
+        WarPlayer player = new WarPlayer(acct);
+        Deck deck = new Deck();
+        player.addPlayerHand(deck.deal(52));
+
+        int expected = 52;
+        int actual = player.playerHand.deckSize();
+
+        Assert.assertEquals(expected, actual);
 
     }
     @Test
     public void testAddPoint(){
+        CasinoAccount acct = new CasinoAccount(null, null);
+        WarPlayer player = new WarPlayer(acct);
+
+        int points = player.getPlayerPoints();
+        int expected = points + 1;
+
+        player.addPoints(1);
+        int actual = player.getPlayerPoints();
+
+        Assert.assertEquals(expected, actual);
+
 
     }
 }
