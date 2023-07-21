b57ca2e35313d82228edd413a61eef1774205fbd
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-21 11:19:17 -0400

    updates

--- src/main/java/com/github/zipcodewilmington/casino/gametools/Card.java
+++ src/main/java/com/github/zipcodewilmington/casino/gametools/Card.java
@@ -25,10 +25,6 @@ public class Card {
     }
 
 
-//    public int getBlackJackValue(){
-//
-//    }
-
 
     //add toString
     @Override
--- src/test/java/com/github/zipcodewilmington/casino/games/war/WarPlayerTest.java
+++ src/test/java/com/github/zipcodewilmington/casino/games/war/WarPlayerTest.java
@@ -2,6 +2,7 @@ package com.github.zipcodewilmington.casino.games.war;
 
 
 import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.gametools.Deck;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -19,11 +20,23 @@ public class WarPlayerTest {
 
     @Test
     public void testGetTopCard(){
+        CasinoAccount acct = new CasinoAccount(null, null);
+        WarPlayer player = new WarPlayer(acct);
 
+        String expected = "TWO of SPADES";
+        String actual = player.getTopCard().toString();
+//        Assert.assertEquals(expected, actual);
+        System.out.println(actual);
     }
 
     @Test
     public void testGetCurrentCard(){
+        Deck deck = new Deck();
+        String expected = "THREE of SPADES";
+        deck.removeTopCard();
+        deck.removeTopCard(); //remove the first and second cards
+//        String actual =
+
 
     }
 
