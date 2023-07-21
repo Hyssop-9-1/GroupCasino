b66dad3789131c5af0c6aca9cf846ef0ff0f9bff
taylor <taylormyers01@comcast.net>
2023-07-21 11:37:18 -0400

    Added some functionality to Card Enums

--- src/main/java/com/github/zipcodewilmington/casino/gametools/Card.java
+++ src/main/java/com/github/zipcodewilmington/casino/gametools/Card.java
@@ -7,6 +7,7 @@ public class Card {
     Suits suit;
     Rank rank;
 
+
     public Card(Suits suit, Rank rank){
         this.suit = suit;
         this.rank = rank;
@@ -29,5 +30,26 @@ public class Card {
     public String toString() {
         return  rank + " of " + suit;
     }
+//    public String cardDisplay(){
+//      // return cardDisplay[suit.ordinal()][rank.ordinal()];
+//    }
+
+
+
+//     .--`----.  .-------.  .------.  .------.
+//     |A_  _ |  | A /\  |  |A _   |  |A .   |
+//     |( \/ )|  |  /  \ |  | ( )  |  | / \  |
+//     | \  / |  |  \  / |  |(_x_) |  |(_,_) |
+//     |  \/ A|  |   \/ A|  |  Y  A|  |  I  A|
+//     `------'  `-------'  `------'  `------'
+
+//    public static void main(String[] args) {
+//        Card card = new Card(Suits.HEARTS,Rank.QUEEN );
+//        //System.out.println(String.format(".------.\n|%-2s_   |\n| ( )  |\n|(_x_) |\n|  Y %-2s|\n`------'\n", "1", "1")); //clubs
+//        //System.out.println(String.format(".------.\n|%-2s  _ |\n|( \\/ )|\n| \\  / |\n|  \\/%-2s|\n`------'\n", "10","10"));//hearts
+//        //System.out.println(String.format(".-------.\n|%-2s /\\  |\n|  /  \\ |\n|  \\  / |\n|   \\/%-2s|\n`-------' \n", "10", "10"));
+//        System.out.println(String.format(".------.\n|%-2s,   |\n| / \\  |\n|(_,_) |\n|  I %-2s|\n`------'", "1", "1"));
+//        //System.out.println(card.rank.getAbbreviation());
+//    }
 }
 
