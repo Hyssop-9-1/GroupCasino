eea6db664dac38cfe7e499c6aaf7083f170a302a
Dave Jourdain <136352147+DaveJourdain@users.noreply.github.com>
2023-07-21 09:39:19 -0400

    added method stubs for roulette class (#1)

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGame.java
@@ -0,0 +1,24 @@
+package com.github.zipcodewilmington.casino.games.roulette;
+
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+public class RouletteGame {
+    String[] wheel;
+
+    public Integer spinWheel() {
+        int index;
+        return null;
+    }
+
+    public Boolean checkWinCond(PlayerInterface player) {
+        boolean winCond = false;
+        return winCond;
+    }
+
+    public Integer calculateWinnings(PlayerInterface player) {
+        int winningAmount;
+        return null;
+    }
+
+
+}
