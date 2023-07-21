5984c7a70d850f0218d763f698a9a5c050a3f5b8
dave <davejourdain@gmail.com>
2023-07-21 09:40:00 -0400

    changes

--- src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGame.java
@@ -1,10 +1,26 @@
 package com.github.zipcodewilmington.casino.games.roulette;
 
+import com.github.zipcodewilmington.Casino;
+import com.github.zipcodewilmington.casino.GameInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.utils.IOConsole;
 
-public class RouletteGame {
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class RouletteGame implements GameInterface {
     String[] wheel;
+    ArrayList < RoulettePlayer > players = new ArrayList<>();
+    IOConsole console = new  IOConsole();
+/*
+    things you can bet on 1-36 that gives them 100 times of whatever they bet
+    or you can bet on a color which is 50/50 double your bets
+    bet between 1-9 or 10-18 or 19-27 or 28-36 that will get you four times whatever you get
+ */
 
+    public RouletteGame(){
+
+    }
     public Integer spinWheel() {
         int index;
         return null;
@@ -20,5 +36,45 @@ public class RouletteGame {
         return null;
     }
 
+    /**
+     *
+     */
+    public static void main(String[] args) {
+        new RouletteGame().run();
+    }
+    @Override
+    public void run() {
+        capturePlayerKey();
 
+
+
+    }
+
+    @Override
+    public void setup() {
+
+
+    }
+
+    @Override
+    public void addPlayer(PlayerInterface player) {
+        this.players.add((RoulettePlayer)player);
+    }
+
+    @Override
+    public void removePlayer() {
+
+    }
+
+    @Override
+    public void checkWinCond() {
+
+    }
+    public int capturePlayerKey(){
+        String question ="press 1: to bet on a number\n" +
+                            "press 2: to bet on a color\n" +
+                            "press 3: to bet odd or even";
+        return console.getIntegerInput(question);
+
+    }
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/roulette/RoulettePlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/RoulettePlayer.java
@@ -1,4 +1,21 @@
 package com.github.zipcodewilmington.casino.games.roulette;
 
-public class RoulettePlayer {
+import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.GamblingPlayer;
+
+public class RoulettePlayer extends GamblingPlayer {
+    int currentBetKey;
+
+    public int getCurrentBetKey() {
+        return currentBetKey;
+    }
+
+    public void setCurrentBetKey(int currentBetKey) {
+        this.currentBetKey = currentBetKey;
+    }
+
+    public RoulettePlayer(CasinoAccount acct) {
+        super(acct);
+
+    }
 }
