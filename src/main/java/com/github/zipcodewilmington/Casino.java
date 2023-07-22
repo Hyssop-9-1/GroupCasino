abd4179f7c259a7ae708c97ab2094ef61c843261
taylor <taylormyers01@comcast.net>
2023-07-22 16:26:07 -0400

    pushing updates that I made to the casino class

--- src/main/java/com/github/zipcodewilmington/Casino.java
+++ src/main/java/com/github/zipcodewilmington/Casino.java
@@ -31,70 +31,68 @@ public class Casino implements Runnable {
         String arcadeDashBoardInput;
         try {
             casinoAccountManager.load();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        do {
-            arcadeDashBoardInput = getArcadeDashboardInput();
-            if ("select-game".equals(arcadeDashBoardInput)) {
-                String accountName = console.getStringInput("Enter your account name:");
-                String accountPassword = console.getStringInput("Enter your account password:");
-                CasinoAccount casinoAccount = casinoAccountManager.getAccount(accountName, accountPassword);
-                boolean isValidLogin = casinoAccount != null;
-                if (isValidLogin) {
-                    String gameSelectionInput = getGameSelectionInput().toUpperCase();
-                    if (gameSelectionInput.equals("SLOTS")) {
-                        SlotsPlayer player = new SlotsPlayer(casinoAccount);
-                        SlotsGame game = new SlotsGame();
-                        game.addPlayer(player);
-                        game.run();
-                    } else if (gameSelectionInput.equals("NUMBERGUESS")) {
-                        NumberGuessPlayer account = new NumberGuessPlayer(casinoAccount);
-                        NumberGuessGame game = new NumberGuessGame();
-                        game.addPlayer(account);
-                        game.run();
-                    } else if (gameSelectionInput.equalsIgnoreCase("war")){
-                        WarPlayer account = new WarPlayer(casinoAccount);
-                        WarGame game = new WarGame();
-                        game.addPlayer(account);
-                        game.run();
 
-                    } else if(gameSelectionInput.equalsIgnoreCase("ROLL")){
+            do {
+                arcadeDashBoardInput = getArcadeDashboardInput();
+                if ("select-game".equals(arcadeDashBoardInput)) {
+                    CasinoAccount casinoAccount = login();
+                    boolean isValidLogin = casinoAccount != null;
+                    if (isValidLogin) {
+                        String gameSelectionInput = getGameSelectionInput().toUpperCase();
+                        if (gameSelectionInput.equals("SLOTS")) {
+                            SlotsPlayer player = new SlotsPlayer(casinoAccount);
+                            SlotsGame game = new SlotsGame();
+                            game.addPlayer(player);
+                            game.run();
+                        } else if (gameSelectionInput.equals("NUMBERGUESS")) {
+                            NumberGuessPlayer account = new NumberGuessPlayer(casinoAccount);
+                            NumberGuessGame game = new NumberGuessGame();
+                            game.addPlayer(account);
+                            game.run();
+                        } else if (gameSelectionInput.equalsIgnoreCase("war")) {
+                            WarPlayer account = new WarPlayer(casinoAccount);
+                            WarGame game = new WarGame();
+                            game.addPlayer(account);
+                            game.run();
 
-                        RollPlayer player = new RollPlayer(casinoAccount);
-                        RollGame game = new RollGame();
-                        game.addPlayer(player);
-                        int hold = console.getIntegerInput("This game requires 2 players, input a number for how many more people would like to play:");
-                        for(int i = 0; i < hold; i++){
-                            RollPlayer player1 = new RollPlayer(login());
-                            game.addPlayer(player1);
+                        } else if (gameSelectionInput.equalsIgnoreCase("ROLL")) {
+
+                            RollPlayer player = new RollPlayer(casinoAccount);
+                            RollGame game = new RollGame();
+                            game.addPlayer(player);
+                            int hold = console.getIntegerInput("This game requires 2 players, input a number for how many more people would like to play:");
+                            for (int i = 0; i < hold; i++) {
+                                RollPlayer player1 = new RollPlayer(login());
+                                game.addPlayer(player1);
+                            }
+                            game.run();
+
+
+                        } else {
+                            // TODO - implement better exception handling
+                            String errorMessage = "[ %s ] is an invalid game selection";
+                            throw new RuntimeException(String.format(errorMessage, gameSelectionInput));
                         }
-                        game.run();
-
-
                     } else {
-                        // TODO - implement better exception handling
-                        String errorMessage = "[ %s ] is an invalid game selection";
-                        throw new RuntimeException(String.format(errorMessage, gameSelectionInput));
+//                    // TODO - implement better exception handling
+//                    String errorMessage = "No account found with name of [ %s ] and password of [ %s ]";
+//                    throw new RuntimeException(String.format(errorMessage, accountPassword, accountName));
                     }
-                } else {
-                    // TODO - implement better exception handling
-                    String errorMessage = "No account found with name of [ %s ] and password of [ %s ]";
-                    throw new RuntimeException(String.format(errorMessage, accountPassword, accountName));
+                } else if ("create-account".equals(arcadeDashBoardInput)) {
+                    createAccount();
+//                console.println("Welcome to the account-creation screen.");
+//                String accountName = console.getStringInput("Enter your account name:");
+//                String accountPassword = console.getStringInput("Enter your account password:");
+//                CasinoAccount newAccount = casinoAccountManager.createAccount(accountName, accountPassword);
+//                casinoAccountManager.registerAccount(newAccount);
                 }
-            } else if ("create-account".equals(arcadeDashBoardInput)) {
-                console.println("Welcome to the account-creation screen.");
-                String accountName = console.getStringInput("Enter your account name:");
-                String accountPassword = console.getStringInput("Enter your account password:");
-                CasinoAccount newAccount = casinoAccountManager.createAccount(accountName, accountPassword);
-                casinoAccountManager.registerAccount(newAccount);
-            }
-        } while (!"logout".equals(arcadeDashBoardInput));
-        try {
+            } while (!"logout".equals(arcadeDashBoardInput));
             casinoAccountManager.save();
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 
     private String getArcadeDashboardInput() {
@@ -126,11 +124,21 @@ public class Casino implements Runnable {
 
     private CasinoAccount login(){
         CasinoAccount account;
+        String errorMessage = "No account found with name of [ %s ] and password of [ %s ]";
+        int loginAttempts = 0;
         do {
             String accountName = console.getStringInput("Enter your account name:");
             String accountPassword = console.getStringInput("Enter your account password:");
             account = casinoAccountManager.getAccount(accountName, accountPassword);
+            loginAttempts ++;
+            if(account == null ){
+                console.println(String.format(errorMessage, accountName, accountPassword));
+                if(loginAttempts%3==0) {
+                    console.println("Failed login 3 times in a row: C - create an account | R - retry login | ");
+                }
+            }
         }while(account == null);
+        for(int i = 0; i < 50; i++){console.println(" ");}
         return account;
     }
     private void createAccount(){
