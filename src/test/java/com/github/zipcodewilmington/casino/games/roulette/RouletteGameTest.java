754b779f14228512f81686abcb2fda33b4b96d63
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-24 22:29:59 -0400

    Review w. Dave

---
+++ src/test/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGameTest.java
@@ -0,0 +1,23 @@
+package com.github.zipcodewilmington.casino.games.roulette;
+
+import org.junit.Test;
+
+public class RouletteGameTest {
+
+    @Test
+    public void testSpinWheel(){
+
+    }
+
+    @Test
+    public void testValidBet(){
+
+
+    }
+
+    @Test void testBetType(){
+
+    }
+
+
+}
